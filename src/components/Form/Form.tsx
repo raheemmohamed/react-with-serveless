ca9202197f3447e4db5dde5294a7b0eb022babb6
Raheem <mohamedraheem0555@gmail.com>
2021-02-03 14:23:27 +0530

    Added form submit with state

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 import "./Form.scss";
+import axios from 'axios';
 
 class Form extends Component {
+  form:any;
+  state:any;
 
   constructor(public props:any){
     super(props);
+
+    this.state = {name: '', email: 'mohamedraheem0444@gmail.com', msg: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event:any) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+    console.log("handle change form event", event.target)
+   // this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event:any) {
+    alert('A name was submitted: ' + this.state);
+    console.log("Submitted form event", event, this.state)
+    event.preventDefault();
+  }
+
 
 
   render() {
     return (
       <div>
         <h4>{this.props.title}</h4>
-        <form className="text-left p-4 border" data-testid="Form">
+        <form className="text-left p-4 border" data-testid="Form" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label>Your Name</label>
-          <input type="text" className="form-control" placeholder="Your name" />
+          <input type="text" className="form-control" placeholder="Your name" name="name" value={this.state.name}  onChange={this.handleChange} />
         </div>
 
         <div className="form-group">
@@ -24,18 +52,20 @@ class Form extends Component {
             type="email"
             className="form-control"
             aria-describedby="emailHelp"
-            placeholder="Enter email"
+            placeholder="Enter email" name="email" value={this.state.email} onChange={this.handleChange}
+            readOnly
           />
           <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
+            This email is cannot be change because it's verified email connected with AWS SES (Simple Email Service)
           </small>
         </div>
         <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
+          <label>Message</label>
+          <textarea
             className="form-control"
-            placeholder="Password"
+            placeholder="Your message here"
+            name="msg"
+            value={this.state.msg} onChange={this.handleChange}
           />
         </div>
 
